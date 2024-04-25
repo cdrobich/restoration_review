d6ac26ee3aabda1a04cbe0ecd1d0cacd7dd4a7f3
Courtney Robichaud <44408770+cdrobich@users.noreply.github.com>
2024-04-25 14:01:36 -0400

    Summing methods and variables

--- data/readMe.md
+++ data/readMe.md
@@ -1,3 +1,7 @@
 # Data Extraction
-- `data_extraction_2023` - evidence map data extraction
 - `data_24April2024_openRefine` - Revised by KG, cleaned up in OpenRefine & duplicates corrected (see Open Refine script in raw data for details)
+- `data/includes_cleanedup_24APR2024.csv` - Only included ('Y') articles
+	- outside OpenRefine: 
+		- A47 changed to 'N' because there is a published version (this is a thesis)
+		- A381 changed to 'N' because there is no comparator
+		- A28 (abiotic), A59 (microbial) and A60 (abiotic) measured outcome variable added
--- scripts/summary_data.R
+++ scripts/summary_data.R
@@ -19,5 +19,38 @@ dup <- distinct(subset)  # remove duplicates
 write.csv(dup, "output/duplicates.csv")
 
 # highlight them in excel and correct the mistakes in Open Refine so there is a recorded script
+# see 'OpenRefine_script_24April2024" in "data"
+
+data_cleaned <- read.csv('data/data_24April2024_openRefine.csv')
+str(data_cleaned)
+
+data_cleaned <- data_cleaned %>% select(ACC_NUM:Other)
+data_cleaned <- janitor::clean_names(data_cleaned)         
+
+data_includes <- data_cleaned %>% filter(include == 'Y')
+write.csv(data_includes,'data/includes_cleanedup_24APR2024.csv', row.names = FALSE)
 
 
+# Measured Outcome Variables ----------------------------------------------
+data_includes <- read.csv('data/includes_cleanedup_24APR2024.csv')
+
+write.csv(data_includes %>% count(measured_outcome), 'data/measure_in_outcomes.csv') 
+outcomes <- data_includes %>% count(measured_outcome)
+
+colnames(data_includes)
+
+outcomes_fig <- ggplot(outcomes, aes(x = reorder(measured_outcome, +n), y = n)) +
+            geom_col() +
+            coord_flip()
+
+
+outcomes_abiotic <- data_includes %>% 
+            filter(measured_outcome == 'Abiotic') %>% 
+            pivot_longer(cols = multivariate_community_composition:hydrology,
+                         names_to = "method",
+                         values_to = "count") %>% 
+            select(method:count) %>% 
+            group_by(method) %>% 
+            summarize(Sum = sum(count))
+
+outcomes_abiotic 
